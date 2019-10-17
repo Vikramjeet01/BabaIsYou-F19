ec4052f3656a9fd93499fd07423bd69ba9613d0a
INDRAVADAN SHRIMALI <indravadanshrimali@hotmail.com>
2019-10-17 12:14:43 -0400

    sprites initialized

--- BabaIsYou-F19/GameScene.swift
+++ BabaIsYou-F19/GameScene.swift
@@ -13,6 +13,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     // MARK: Outlets for sprites
     var player:SKSpriteNode!
+    var isblock1:SKSpriteNode!
+    var stopblock:SKSpriteNode!
+    var flagblock:SKSpriteNode!
+    var flag:SKSpriteNode!
+    var wallblock:SKSpriteNode!
+    var winblock:SKSpriteNode!
+    var isblock2:SKSpriteNode!
+    var wallL:SKSpriteNode!
+    var wallR:SKSpriteNode!
+    var wallT:SKSpriteNode!
+    var wallB:SKSpriteNode!
     let PLAYER_SPEED:CGFloat = 20
 
     override func didMove(to view: SKView) {
@@ -20,16 +31,33 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         // initialze the player
         self.player = self.childNode(withName: "player") as! SKSpriteNode
-    }
+        self.isblock1 = self.childNode(withName: "isblock1") as! SKSpriteNode
+        self.stopblock = self.childNode(withName: "stopblock") as! SKSpriteNode
+        self.flagblock = self.childNode(withName: "flagblock") as! SKSpriteNode
+        self.flag = self.childNode(withName: "flag") as! SKSpriteNode
+        self.wallblock = self.childNode(withName: "wallblock") as! SKSpriteNode
+        self.winblock = self.childNode(withName: "winblock") as! SKSpriteNode
+        self.isblock2 = self.childNode(withName: "isblock2") as! SKSpriteNode
+        self.wallL = self.childNode(withName: "wallL") as! SKSpriteNode
+        self.wallR = self.childNode(withName: "wallR") as! SKSpriteNode
+        self.wallT = self.childNode(withName: "wallT") as! SKSpriteNode
+        self.wallB = self.childNode(withName: "wallB") as! SKSpriteNode
+
+        }
    
     func didBegin(_ contact: SKPhysicsContact) {
         print("Something collided!")
         
         let nodeA = contact.bodyA.node
         let nodeB = contact.bodyB.node
+        //let nodeC = contact.bodyC.node
         
-        if (nodeA == nil || nodeB == nil) {
+        if (nodeA == nil || nodeB == nil ) {
             return
+        }
+        
+        if (nodeA!.name == "player" && nodeB!.name == "flag") {
+            print("Resul: You WIn")
         }
     }
     
@@ -49,7 +77,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // WHAT NODE DID THE PLAYER TOUCH
         // ----------------------------------------------
         let nodeTouched = atPoint(location).name
-        print("Player touched: \(nodeTouched)")
+        //print("Player touched: \(nodeTouched)")
         
         if (nodeTouched == "up") {
             // move up
